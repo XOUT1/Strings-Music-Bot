ded9ba224a6cd592b8606f1ce52359dee3131d86
iwin siju <77713114+XOUT1@users.noreply.github.com>
2022-02-24 14:11:04 +0300

    nvm

--- commands/🎶 Music/play.js
+++ commands/🎶 Music/play.js
@@ -64,12 +64,3 @@ module.exports = {
     }
   }
 };
-/**
- * @INFO
- * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
- * @INFO
- * Work for Milrato Development | https://milrato.dev
- * @INFO
- * Please mention Him / Milrato Development, when using this Code!
- * @INFO
- */
